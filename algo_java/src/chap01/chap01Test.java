4022709a91df38964d9edd0549a1a7d22a59d7c5
NotableDeveloper <castlereal@naver.com>
2023-02-27 23:01:58 -0800

    <test> : 챕터 1 테스트 코드 최초 작성 챕터 1의 모든 문제(1번 ~ 12번 문제)에 대한 테스트 코드의 프로토 타입 작성

---
+++ algo_java/src/chap01/chap01Test.java
@@ -0,0 +1,66 @@
+package chap01;
+
+import org.junit.jupiter.api.Test;
+
+public class chap01Test {
+    @Test
+    void problem01(){
+
+    }
+
+    @Test
+    void problem02(){
+
+    }
+
+    @Test
+    void problem03(){
+
+    }
+
+    @Test
+    void problem04(){
+
+    }
+
+    @Test
+    void problem05(){
+
+    }
+
+    @Test
+    void problem06(){
+
+    }
+
+    @Test
+    void problem07(){
+
+    }
+
+    @Test
+    void problem08(){
+
+    }
+
+
+    @Test
+    void problem09(){
+
+    }
+
+    @Test
+    void problem10(){
+
+    }
+
+    @Test
+    void problem11(){
+
+    }
+
+    @Test
+    void problem12(){
+
+    }
+}
